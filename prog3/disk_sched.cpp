36743dbe51cd4c5372cc1175872cf01c1f43b995
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2017-04-24 16:23:07 -0600

    Added usage.

--- prog3/disk_sched.cpp
+++ prog3/disk_sched.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello";
+    if (argc != 2)
+    {
+        cout << "Usage: ./disk_sched disk-head-position\n";
+        exit(0);
+    }   
+
+    // int requests[1000];
+    // fill with RAND%5000
+
     return 0;
 }
