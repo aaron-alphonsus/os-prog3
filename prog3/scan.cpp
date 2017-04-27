5f395c450a8ed5aa920b2d053f982e44de2f5fa7
Aaron Alphonsus <aaron.alphonsus@gmail.com>
2017-04-26 22:35:20 -0600

    Completed scan (math).

--- prog3/scan.cpp
+++ prog3/scan.cpp
@@ -4,72 +4,33 @@
 
 #include "disk_sched.h"
 
-using namespace std;
+using namespace std; // comment out
 
 int scan(int initial_pos, int request[REQUESTS])
 {
-    vector<int> down;
-    vector<int> up;
+    int head_movement = 0;    
+    int lowest = CYLINDERS;
+    int highest = -1;
 
-    int head_movement = 0;
-    int current_pos = abs(initial_pos);
-    // cerr << "Current pos = " << current_pos << endl;
-    
-    /// Place requests into an 'up' and 'down' vector which will be sorted
     for(int i = 0; i < REQUESTS; i++)
     {
-        if(request[i] < current_pos)
-            down.push_back(request[i]);
-        else if(request[i] > current_pos)
-            up.push_back(request[i]);
-        /// Assumes that == are processed first, therefore 0 head movement
-    } 
-    /// Sort 'down' in descending order and 'up' in ascending
-    sort(down.rbegin(), down.rend());
-    sort(up.begin(), up.end());
-    
-    if(initial_pos < 0)
-    {
-        if(up.size() != 0)
-            down.push_back(0);
-        /// Service the 'down' array
-        for(int i = 0; i < down.size(); i++)
-        {
-            head_movement += abs(current_pos - down[i]);
-            current_pos = down[i]; 
-        }
-        /// Service 'up' array
-        for(int i = 0; i < up.size(); i++)
-        {
-            head_movement += abs(current_pos - up[i]);
-            current_pos = up[i];
-        }     
+        if(request[i] < lowest)
+            lowest = request[i];
+        if(request[i] > highest)
+            highest = request[i]; 
     }
-    else
-    {
-        if(down.size() != 0)
-            up.push_back(CYLINDERS - 1);
-        /// Service 'up' array
-        for(int i = 0; i < up.size(); i++)
-        {
-            head_movement += abs(current_pos - up[i]);
-            current_pos = up[i];
-        } 
-        /// Service the 'down' array
-        for(int i = 0; i < down.size(); i++)
-        {
-            head_movement += abs(current_pos - down[i]);
-            current_pos = down[i]; 
-        }
-    }
+    // cerr << "Lowest = " << lowest;
+    // cerr << "Highest = " << highest;
 
-    for(int i = 0; i < down.size(); i++)
-        cerr << down[i] << " ";
-    cerr << "\n";
-
-    for(int i = 0; i < up.size(); i++)
-        cerr << up[i] << " ";
-    cerr << "\n";
-
+    if(initial_pos <= 0 && highest <= abs(initial_pos))
+        head_movement = abs(initial_pos) - lowest;
+    else if(initial_pos <= 0 && highest > abs(initial_pos))
+        head_movement = abs(initial_pos) + highest;
+    else if(initial_pos >= 0 && lowest >= abs(initial_pos))
+        head_movement = highest - abs(initial_pos);
+    else if(initial_pos >= 0 && lowest < abs(initial_pos))
+        head_movement = ((CYLINDERS - 1) - abs(initial_pos)) 
+                        + ((CYLINDERS - 1) - lowest);   
+     
     return head_movement; 
 }
